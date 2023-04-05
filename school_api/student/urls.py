1bbb3b0f7da014fb0ca6f69578b383c6612e55cd
Bhupendra Dodiya <bdbhupendra123@gmail.com>
2023-04-05 10:00:10 +0530

    create student api using generics method

--- school_api/student/urls.py
+++ school_api/student/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+from student import views
+
 urlpatterns = [
-    
+    path('',views.student_get.as_view()),
+    path('one/<int:pk>/',views.student_one.as_view()),
+    path('create/',views.student_create.as_view()),
+    path('update/<int:pk>/',views.student_update.as_view()),
+    path('delete/<int:pk>/',views.student_delete.as_view()),
 ]
--- school_api/student/views.py
+++ school_api/student/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import StudentSerializer
+from .models import Student
 
 # Create your views here.
+class student_get(generics.ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class student_one(generics.RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class student_create(generics.CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class student_update(generics.UpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class student_delete(generics.DestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
